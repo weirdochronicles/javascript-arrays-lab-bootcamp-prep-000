bd2304206c82df14e3c4db415d398a44b19c6d28
Emma Goodman <emmagoodmannyc@gmail.com>
2018-04-17 20:33:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
 const app = "I don't do much."
 
-var kittens = ['Milo', 'Otis', 'Garfield']
+var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(name){
   kittens.push(name)
+  return kittens
 }
 
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
+  return kittens
 }
 
-function destructivelyRemoveLastKitten(){
-  kittens.pop()
+function destructivelyRemoveLastKitten(name){
+  return kittens.pop()
 }
 
 function destructivelyRemoveFirstKitten(){
-  kittens.shift()
+  kittens.shift(name)
+  return kittens
 }
 
 function appendKitten(name){
-  return [...kittens, name]
-}
-
-function prependKitten(name){
   return [name, ...kittens]
-}
-
-function removeFirstKitten(){
-  return kittens.slice(1)
-}
-
-function removeLastKitten() {
-  return kittens.slice(0, kittens.length - 1)
 }
